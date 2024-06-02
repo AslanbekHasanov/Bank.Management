fda7a9b11f833c8b7952b56b5544f2a4d4d51337
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-02 23:47:56 +0500

    DATA: Create User Model

---
+++ Bank.Management.Console/Models/User.cs
@@ -0,0 +1,11 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+
+namespace Bank.Management.Console.Models
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
