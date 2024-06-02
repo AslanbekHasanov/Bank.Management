5e20422ddf02cecb99afa463d9eb4c02973eab4e
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-02 23:43:03 +0500

    BROKER: Impliment Logging Broker And Add File Headr

---
+++ Bank.Management.Console/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+
+namespace Bank.Management.Console.Brokers.Loggings
+{
+    internal interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogError(string userMessage);
+        void LogError(Exception exception);
+    }
+}
---
+++ Bank.Management.Console/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+
+namespace Bank.Management.Console.Brokers.Loggings
+{
+    internal class LoggingBroker : ILoggingBroker
+    {
+        public void LogInformation(string message)
+        {
+            System.Console.ForegroundColor = ConsoleColor.DarkGreen;
+            System.Console.WriteLine(message);
+            System.Console.ResetColor();
+        }
+        public void LogError(string userMessage)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(userMessage);
+            System.Console.ResetColor();
+        }
+        public void LogError(Exception exception)
+        {
+            System.Console.ForegroundColor = ConsoleColor.DarkRed;
+            System.Console.WriteLine(exception.Message);
+            System.Console.ResetColor();
+        }
+    }
+}
