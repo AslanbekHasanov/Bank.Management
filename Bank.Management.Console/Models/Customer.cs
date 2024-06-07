fc5c1c30e6b37e3c86122dc599198e90b9653bd6
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-07 16:24:05 +0500

    DATA: Initialize Customer And BankAccountNumber

---
+++ Bank.Management.Console/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+namespace Bank.Management.Console.Models
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public decimal AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
