b418df1db9949ef1b8ee8c249a001d47d74255f5
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-07 15:10:33 +0500

    FOUNDATION: Imliment Registr Service

---
+++ Bank.Management.Console/Services/Foundations/Registrs/IRegistrService.cs
@@ -0,0 +1,14 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using Bank.Management.Console.Models;
+
+namespace Bank.Management.Console.Services.Foundations.Registrs
+{
+    internal interface IRegistrService
+    {
+        bool LogIn(User user);
+        User SigUp(User user);
+    }
+}
---
+++ Bank.Management.Console/Services/Foundations/Registrs/RegistrService.cs
@@ -0,0 +1,31 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using Bank.Management.Console.Brokers.Loggings;
+using Bank.Management.Console.Brokers.Storages;
+using Bank.Management.Console.Models;
+
+namespace Bank.Management.Console.Services.Foundations.Registrs
+{
+    internal class RegistrService : IRegistrService
+    {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IRegistrBroker registrBroker;
+
+        public RegistrService() 
+        {
+            this.loggingBroker = new LoggingBroker();
+            this.registrBroker = new RegistrBroker();
+        }
+        public bool LogIn(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User SigUp(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
