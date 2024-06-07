67f99b60c8c2fccf51ac1c6afde67f1a628f6cae
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-07 15:38:58 +0500

    DATA: Impliment LogIn Method and Validation

--- Bank.Management.Console/Services/Foundations/Registrs/RegistrService.cs
+++ Bank.Management.Console/Services/Foundations/Registrs/RegistrService.cs
@@ -5,6 +5,7 @@
 using Bank.Management.Console.Brokers.Loggings;
 using Bank.Management.Console.Brokers.Storages;
 using Bank.Management.Console.Models;
+using System.Runtime.InteropServices;
 
 namespace Bank.Management.Console.Services.Foundations.Registrs
 {
@@ -20,12 +21,53 @@ namespace Bank.Management.Console.Services.Foundations.Registrs
         }
         public bool LogIn(User user)
         {
-            throw new NotImplementedException();
+            return user is null
+                ? InvalidLogInUser()
+                : LogInUserValidation(user);
         }
 
         public User SigUp(User user)
         {
             throw new NotImplementedException();
+        }
+
+        private bool LogInUserValidation(User user)
+        {
+            if (String.IsNullOrWhiteSpace(user.Name)
+                || String.IsNullOrWhiteSpace(user.Password))
+            {
+                this.loggingBroker.LogInformation("User data is not required.");
+                return false;
+            }
+            else
+            {
+                bool isLogIn = this.registrBroker.LogIn(user);
+
+                if (user.Password.Length >= 8)
+                {
+                    if (isLogIn is true)
+                    {
+                        this.loggingBroker.LogInformation("Successfully logged in.");
+                        return true;
+                    }
+                    else
+                    {
+                        this.loggingBroker.LogError("User does not exist in the database.");
+                        return false;
+                    }
+                }
+                else
+                {
+                    this.loggingBroker.LogError("Password does not contain 8 characters.");
+                    return false;
+                }
+            }
+        }
+
+        private bool InvalidLogInUser()
+        {
+            this.loggingBroker.LogError("User data is null.");
+            return false;
         }
     }
 }
