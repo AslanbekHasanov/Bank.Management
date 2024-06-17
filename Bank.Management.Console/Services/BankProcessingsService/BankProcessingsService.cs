305d08cb017fd3db02202bd332807f46b2280fa1
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-17 15:56:03 +0500

    PROCESSINGS: Impliment Bank Processings Service

---
+++ Bank.Management.Console/Services/BankProcessingsService/BankProcessingsService.cs
@@ -0,0 +1,60 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using Bank.Management.Console.Brokers.Storages.RegistrsStorage;
+using Bank.Management.Console.Models;
+using Bank.Management.Console.Services.BankProcessingsService;
+using Bank.Management.Console.Services.Foundations.Banks;
+using Bank.Management.Console.Services.Foundations.Banks.Customers;
+using Bank.Management.Console.Services.Foundations.Registrs;
+
+namespace Bank.Management.Console.Services.BankProcessing
+{
+    internal class BankProcessingsService : IBankProcessingsService
+    {
+        private readonly IRegistrService registrService;
+        private readonly IBankService bankService;
+        private readonly ICustomerService customerService;
+
+        public BankProcessingsService(
+            IRegistrService registrService,
+            IBankService bankService,
+            ICustomerService customerService)
+        {
+            this.registrService = registrService;
+            this.bankService = bankService;
+            this.customerService = customerService;
+        }
+
+        public bool DeleteForClient(decimal accountNumber) =>
+            this.customerService.DeleteClient(accountNumber);
+
+        public decimal GetBalance(decimal accountNumberForBank) =>
+            this.bankService.GetBalanceInBank(accountNumberForBank);
+
+        public decimal GetMoney(decimal accountNumberForBank, decimal balance) =>
+            this.bankService.GetMoney(accountNumberForBank, balance);
+
+        public bool LogInUser(User user) =>
+            this.registrService.LogIn(user);
+
+        public bool PostDeposit(decimal accountNumberForBank, decimal balance) =>
+            this.bankService.AddDeposit(accountNumberForBank, balance);
+
+        public bool PostForClient(Customer customer) =>
+            this.customerService.CreateClient(customer);
+
+        public User PostUser(User user) =>
+            this.registrService.SignUp(user);
+
+        public bool TransferMoneyBetweenAccountsForClient(
+            decimal firstAccountNumber,
+            decimal secondAccountNumber,
+            decimal money) =>
+            this.customerService.TransferMoneyBetweenClients(
+                            firstAccountNumber,
+                            secondAccountNumber,
+                            money);
+    }
+}
---
+++ Bank.Management.Console/Services/BankProcessingsService/IBankProcessingsService.cs
@@ -0,0 +1,23 @@
+﻿//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using Bank.Management.Console.Models;
+
+namespace Bank.Management.Console.Services.BankProcessingsService
+{
+    internal interface IBankProcessingsService
+    {
+        public bool DeleteForClient(decimal accountNumber);
+        public decimal GetBalance(decimal accountNumberForBank);
+        public decimal GetMoney(decimal accountNumberForBank, decimal balance);
+        public bool LogInUser(User user);
+        public bool PostDeposit(decimal accountNumberForBank, decimal balance);
+        public bool PostForClient(Customer customer);
+        public User PostUser(User user);
+        public bool TransferMoneyBetweenAccountsForClient(
+                decimal firstAccountNumber,
+                decimal secondAccountNumber,
+                decimal money); 
+    }
+}
